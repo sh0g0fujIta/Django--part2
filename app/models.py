d3fbae67bdcc0b58eb0d532b62fbb509b726dc99
sh0g0fujIta <kanayokokami@yahoo.co.jp>
2023-05-14 12:19:21 +0900

    second commit

--- app/models.py
+++ app/models.py
@@ -6,5 +6,6 @@ class User(models.Model):
     email = models.EmailField('メールアドレス', max_length=100)
     phone = models.IntegerField('電話番号', blank=True, null=True)
 
-    # def __str__(self):
-    #     return self.name
+# 管理画面のオブジェクト表示を登録した名前で表示させる。
+    def __str__(self):
+        return self.name
